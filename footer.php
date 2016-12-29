f9c83170bc31f8c847d2ee614b913d068bfe8405
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-12-29 15:34:00 +0200

    Remove rel="designer" from the footer.

--- footer.php
+++ footer.php
@@ -29,7 +29,7 @@
 			<div class="site-info">
 				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'munsa-lite' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'munsa-lite' ), 'WordPress' ); ?></a>
 				<span class="sep"> | </span>
-				<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'munsa-lite' ), 'Munsa Lite', '<a href="https://foxland.fi/" rel="designer">Foxland</a>' ); ?>
+				<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'munsa-lite' ), 'Munsa Lite', '<a href="https://foxland.fi/">Foxland</a>' ); ?>
 			</div><!-- .site-info -->
 			
 			<?php get_template_part( 'menus/menu', 'social' ); // Loads the menus/menu-social.php template. ?>
